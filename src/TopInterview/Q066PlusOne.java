82ac01395fcc6844dbefcb942ebb3cb9cb08186d
nick dev <nickdev1024@gmail.com>
2021-07-12 17:57:30 +1000

    update code problem 066

--- src/TopInterview/Q066PlusOne.java
+++ src/TopInterview/Q066PlusOne.java
@@ -2,7 +2,7 @@ package TopInterview;
 
 class Q066PlusOne {
 
-    public int[] plusOne(int[] digits) {
+    public int[] plusOne1(int[] digits) {
         int len = digits.length;
         int[] out = new int[len + 1];
         int increment = 0;
@@ -24,6 +24,48 @@ class Q066PlusOne {
         return digits;
     }
 
+    public int[] plusOne2(int[] digits) {
+        int len = digits.length;
+        int[] out = new int[len + 1];
+        int increment = 0;
+        int value;
+        int base = 10;
+        for (int i = len - 1; i >= 0; --i) {
+            value = increment + digits[i];
+            if (i == len - 1) {
+                value += 1;
+            }
+            out[i + 1] = value % base;
+            increment = value / base;
+        }
+        if (increment == 1) {
+            out[0] = 1;
+            return out;
+        }
+        System.arraycopy(out, 1, digits, 0, len);
+        return digits;
+    }
+
+    public int[] plusOne(int[] digits) {
+        int len = digits.length;
+        int i = len - 1;
+        boolean done = false;
+        while (i >= 0 && !done) {
+            if (digits[i] < 9) {
+                ++digits[i];
+                done = true;
+            } else {
+                digits[i--] = 0;
+            }
+        }
+        if (done) {
+            return digits;
+        }
+        int[] out = new int[len + 1];
+        out[0] = 1;
+        return out;
+    }
+
     private void printArray(int[] arr) {
         if (null == arr || arr.length == 0) {
             return;
@@ -37,16 +79,20 @@ class Q066PlusOne {
         System.out.println();
     }
 
-    static public void main(String... args) {
+    private void validate() {
         int[][] data = {
                 {1, 2, 3},
                 {4, 3, 2, 1},
                 {9, 9, 9, 9},
                 {0}
         };
-        Q066PlusOne obj = new Q066PlusOne();
         for (int[] num : data) {
-            obj.printArray(obj.plusOne(num));
+            printArray(plusOne(num));
         }
+    }
+
+    static public void main(String... args) {
+        Q066PlusOne obj = new Q066PlusOne();
+        obj.validate();
     }
 }
